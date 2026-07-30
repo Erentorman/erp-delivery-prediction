b530db8073c883c8eed44f08968861b26ba7b78b
Eren Torman <erentorman03@gmail.com>
2026-07-30 09:58:45 +0300

    feat: add initial persistence model and migration

---
+++ src/App.Persistence/AppDbContext.cs
@@ -0,0 +1,22 @@
+using App.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace App.Persistence;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<User> Users => Set<User>();
+    public DbSet<Role> Roles => Set<Role>();
+    public DbSet<UserRole> UserRoles => Set<UserRole>();
+    public DbSet<SystemSetting> SystemSettings => Set<SystemSetting>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+    }
+}
